b1c2045d4df2b35682cf9abcddeb39dec623530d
Manuel Schneider <manuelschneid3r@gmail.com>
2025-10-14 13:02:48 +0200

    chore(release): v4.3.0

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ from xml.etree import ElementTree
 from albert import *
 
 md_iid = "4.0"
-md_version = "4.2"
+md_version = "4.3.0"
 md_name = "Jetbrains projects"
 md_description = "Open your JetBrains projects"
 md_license = "MIT"
